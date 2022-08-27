090c291a7ebd2aed42d655d52d38650ecaa791d2
Aliaksandr Kudrautsau <sany199574@gmail.com>
2022-08-27 18:47:29 +0300

    Реализовал все Методы действия в PhonebookController

--- src/PS.Phonebook/PS.Phonebook.Web/Controllers/PhonebookController.cs
+++ src/PS.Phonebook/PS.Phonebook.Web/Controllers/PhonebookController.cs
@@ -1,36 +1,116 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PS.Phonebook.Domain.ViewModels;
 using PS.Phonebook.Service.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PS.Phonebook.Web.Controllers
 {
     public class PhonebookController : Controller
     {
         private readonly IEmployeesPhonebookService _dbContext;
+        public PhonebookController(IEmployeesPhonebookService dbContext) => _dbContext = dbContext;
 
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var response = await _dbContext.GetAllEmployeesPhonebooksAsync();
+
+            if(response.StatusCode != Domain.Enums.StatusCode.OK)
+            {
+                return RedirectToAction("Error");
+            }
+
+            return View(response.Data);
         }
 
+
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
-            return View();
+            var model = new EmployeesPhonebookViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Create(EmployeesPhonebookViewModel model)
+        {
+            if (!ModelState.IsValid) { return View(model); }
+
+            await _dbContext.CreateEmployeesPhonebookAsync(model);
+
+            return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Delete()
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int id)
         {
-            return View();
+            var response = await _dbContext.GetEmployeePhonebookAsync(id);
+            if(response.StatusCode != Domain.Enums.StatusCode.OK)
+            {
+                return RedirectToAction("Error");
+            }
+
+            return View(response.Data);
         }
 
-        public IActionResult Edit()
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(EmployeesPhonebookViewModel model)
         {
-            return View();
+            if (!ModelState.IsValid) { return View(model); }
+
+            await _dbContext.UpdateEmployeesPhonebookAsync(model);
+
+            return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Details()
+
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int id)
         {
-            return View();
+            var response = await _dbContext.GetEmployeePhonebookAsync(id);
+
+            if(response.StatusCode != Domain.Enums.StatusCode.OK)
+            {
+                return RedirectToAction("Error");
+            }
+
+            return View(response.Data);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(EmployeesPhonebookViewModel model)
+        {
+            if (ModelState.IsValid) { return View(model); }
+
+            await _dbContext.DeleteEmployeesPhonebookAsync(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var response = await _dbContext.GetEmployeePhonebookAsync(id);
+
+            if(response.StatusCode != Domain.Enums.StatusCode.OK)
+            {
+                return RedirectToAction("Error");
+            }
+
+            return View(response.Data);
         }
     }
 }
