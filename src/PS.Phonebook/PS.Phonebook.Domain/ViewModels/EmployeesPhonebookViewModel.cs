53696bf848b9e77411c5a1c5fe7b76ec8a936b84
Aliaksandr Kudrautsau <sany199574@gmail.com>
2022-08-28 21:22:50 +0300

    Установлена валидация данных

--- src/PS.Phonebook/PS.Phonebook.Domain/ViewModels/EmployeesPhonebookViewModel.cs
+++ src/PS.Phonebook/PS.Phonebook.Domain/ViewModels/EmployeesPhonebookViewModel.cs
@@ -7,17 +7,24 @@ namespace PS.Phonebook.Domain.ViewModels
         public int Id { get; set; }
 
         #region Organization
+        [Required]
         [Display(Name = "Организация")]
         public string OrganizationName { get; set; } = "МИНИСТЕРСТВО ТРАНСПОРТА И КОММУНИКАЦИЙ";
         [Display(Name = "Организация1")]
         public string? Organization1 { get; set; }
         [Display(Name = "Организация2")]
-        public string? Organization2 { get; set; }
+        public string? Organization2 { get; set; }       
         [Display(Name = "Организация3")]
         public string? Organization3 { get; set; }
+
+        [Required]
+        [Range(100000, 999999)]
         [Display(Name = "Индекс")]
         public int Index { get; set; }
+        [StringLength(50,ErrorMessage = "Недопустимый лемит символов")]
         [Display(Name = "Адрес")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string Address { get; set; } = null!;
         [Display(Name = "факс 1")]
         public string Fax1 { get; set; } = null!;
@@ -25,6 +32,7 @@ namespace PS.Phonebook.Domain.ViewModels
         public string Fax2 { get; set; } = null!;
         [Display(Name = "Код АМТС")]
         public string CodeAMTC { get; set; } = null!;
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "E-mail")]
         public string Email { get; set; } = null!;
         [Display(Name = "Сайт")]
@@ -39,15 +47,23 @@ namespace PS.Phonebook.Domain.ViewModels
 
 
         #region Employee
+        [Required]
         [Display(Name = "Фамилия")]
+        [StringLength(50, ErrorMessage = "Недопустимый лемит символов")]
         public string Surname { get; set; } = null!;
+        [Required]
         [Display(Name = "Имя")]
+        [StringLength(50, ErrorMessage = "Недопустимый лемит символов")]
         public string FirstName { get; set; } = null!;
+        [Required]
         [Display(Name = "Отчество")]
+        [StringLength(50, ErrorMessage = "Недопустимый лемит символов")]
         public string Patronymic { get; set; } = null!;
         [Display(Name = "тел.сот.1")]
+        [DataType(DataType.PhoneNumber)]
         public string SellularPhone1 { get; set; } = null!;
         [Display(Name = "тел.сот.2")]
+        [DataType(DataType.PhoneNumber)]
         public string SellularPhone2 { get; set; } = null!;
         #endregion
 
